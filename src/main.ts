f472dc0983c90b4f284986d3ea3bb204a0d7cd28
KuprIvan <ivankuperstudy@gmail.com>
2023-11-02 09:45:57 +0100

    feat: add Routing to the app

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { provideRouter} from "@angular/router";
 import routeConfig from './app/routes';
 
-import { AppModule } from './app/app.module';
 import { bootstrapApplication,provideProtractorTestingSupport } from '@angular/platform-browser';
 import {AppComponent} from "./app/app.component";
 
